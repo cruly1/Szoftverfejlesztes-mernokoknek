427ca4fdd288fb3ef547ce0deafe46a2f61e41b6
cruly1 <pavai.viktor2@gmail.com>
2024-10-17 21:42:02 +0200

    added mockito test cases for playercontroller

--- backend/src/test/java/com/unideb/inf/sfm/SzoftverfejlesztesMernokoknek/SzoftverfejlesztesMernokoknekApplicationTests.java
+++ backend/src/test/java/com/unideb/inf/sfm/SzoftverfejlesztesMernokoknek/SzoftverfejlesztesMernokoknekApplicationTests.java
@@ -2,8 +2,10 @@ package com.unideb.inf.sfm.SzoftverfejlesztesMernokoknek;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
 class SzoftverfejlesztesMernokoknekApplicationTests {
 
 	@Test
